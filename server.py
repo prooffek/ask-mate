179ff7f62f5d29bca4652384d3cd319f723215c2
Kamil Trąba <prooffek@gmail.com>
2021-01-01 10:21:21 +0100

    display images fixed

--- server.py
+++ server.py
@@ -106,7 +106,8 @@ def display_a_question(question_id):
     relevant_answers_dicts = data_manager.find_by_id(question_id, data_manager.LIST_OF_ANSWERS)
     relevant_answers_dicts = data_manager.sort_answers(relevant_answers_dicts)
     # img_path = data_manager.get_image_path()
-    return render_template("display_question.html", question=question_dict, answers=relevant_answers_dicts)
+    return render_template("display_question.html", question=question_dict, answers=relevant_answers_dicts,
+                           img_path=connection.IMAGE_PATH)
     # question_dict = data_manager.find_by_id(question_id, data_manager.LIST_OF_QUESTIONS)[0]
     # relevant_answers_dicts = data_manager.find_by_id(question_id, data_manager.LIST_OF_ANSWERS)
     # num_of_questions = len(data_manager.LIST_OF_QUESTIONS)
